20cc5aab8140522d64f1d6eb9ce0de13fc698201
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-12-04 10:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-  
+
+
+const getSum = () => 
+{
+	const prices=document.querySelectorAll('.price');
+	let sum=0;
+
+	 prices.forEach(function(priceElement) {
+                sum += parseFloat(priceElement.textContent); 
+            });
+	
+	const table=document.querySelector('table');
+	const r = document.createElement("tr");
+	const totalcell=document.createElement("td");
+
+	totalcell.colSpan=2;
+	totalcell.textContent=`${sum}`;
+	
+	r.appendChild(totalcell);
+	table.appendChild(r);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
